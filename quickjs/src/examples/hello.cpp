6a86765f412ad34afcf561c0d83d0517207d5f3e
taylorcyang <taylorcyang@tencent.com>
2021-03-31 12:47:56 +0800

    add latest QuickJS source code

---
+++ quickjs/src/examples/hello.cpp
@@ -0,0 +1,11 @@
+// Copyright 2021 taylorcyang@tencent.com
+
+/**
+ * <pre>
+ * Author: taylorcyang@tencent.com
+ * Date:   2021-03-30
+ * Time:   15:13
+ * Life with Passion, Code with Creativity.
+ * </pre>
+ */
+
